ef26cc79bdd2d14990c143790a78f2b50373764d
DefinitePurple <danieljfitzpatrick3696@gmail.com>
2018-09-27 14:13:41 +0100

    Cellular automaton

--- worksheets/1-basic-html-css-dom-api/cellular-automaton/script.js
+++ worksheets/1-basic-html-css-dom-api/cellular-automaton/script.js
@@ -4,17 +4,18 @@ var max = 100;
 var min = 1;
 var grid = [];
 var parent = document.getElementById('grid');
-
+var width = 101;
+var height = 50;
 
 function drawCells() {
 	console.log(grid);
 	for(var i = 0; i < grid.length; i++){
 		var row = document.createElement("div");
 		parent.appendChild(row);
-		for (var j = 0; j < 101; j++) { 
+		for (var j = 0; j < width; j++) { 
 	    	var div = document.createElement("div");
 			div.classList.add('cell');
-			if (grid[i][j]== 0)
+			if (grid[i][j] == 1)
 				div.style.backgroundColor = active;
 			else 
 				div.style.backgroundColor = inactive;
@@ -25,29 +26,31 @@ function drawCells() {
 }
 
 function generateRows(){
-	for (var i = 0; i < 50; i++) { 
+	for (var i = 0; i < height; i++) { 
 		grid[i] = new Array();
-		for(var j = 0; j < 101; j++){
+		for(var j = 0; j < width; j++){
 			if (i == 0) {
 				grid[i].push(randomInteger());
 			} else {
-				console.log("I am I " + i + " I am J " + j)
-				if 	(grid[i-1][j-1] == 1 && grid[i-1][j] == 1 && grid[i-1][j+1] == 1)
-		 			grid[i].push(0);
-				else if (grid[i-1][j-1] == 1 && grid[i-1][j] == 1 && grid[i-1][j+1] == 0)
+				left = (j - 1 < 0) ? width - 1 : j - 1;
+				right = (j + 1 > width - 1) ? 0 : j + 1; 
+				if 	(grid[i-1][left] == 1 && grid[i-1][j] == 1 && grid[i-1][right] == 1) {
 					grid[i].push(0);
-				else if (grid[i-1][j-1] == 1 && grid[i-1][j] == 0 && grid[i-1][j+1] == 1)
-					grid[i].push(1);
-				else if (grid[i-1][j-1] == 1 && grid[i-1][j] == 0 && grid[i-1][j+1] == 0)
-					grid[i].push(1);
-				else if (grid[i-1][j-1] == 0 && grid[i-1][j] == 1 && grid[i-1][j+1] == 1)
-					grid[i].push(1);
-				else if (grid[i-1][j-1] == 0 && grid[i-1][j] == 1 && grid[i-1][j+1] == 0)
-					grid[i].push(1);
-				else if (grid[i-1][j-1] == 0 && grid[i-1][j] == 0 && grid[i-1][j+1] == 1)
+				} else if (grid[i-1][left] == 1 && grid[i-1][j] == 1 && grid[i-1][right] == 0) {
 					grid[i].push(0);
-				else if (grid[i-1][j-1] == 0 && grid[i-1][j] == 0 && grid[i-1][j+1] == 0)
+				} else if (grid[i-1][left] == 1 && grid[i-1][j] == 0 && grid[i-1][right] == 1) {
+					grid[i].push(1);
+				} else if (grid[i-1][left] == 1 && grid[i-1][j] == 0 && grid[i-1][right] == 0) {
+					grid[i].push(1);
+				} else if (grid[i-1][left] == 0 && grid[i-1][j] == 1 && grid[i-1][right] == 1) {
+					grid[i].push(1);
+				} else if (grid[i-1][left] == 0 && grid[i-1][j] == 1 && grid[i-1][right] == 0) {
+					grid[i].push(1);
+				} else if (grid[i-1][left] == 0 && grid[i-1][j] == 0 && grid[i-1][right] == 1) {
 					grid[i].push(0);
+				} else if (grid[i-1][left] == 0 && grid[i-1][j] == 0 && grid[i-1][right] == 0) {
+					grid[i].push(0);
+				}
 			}
 		}
 	}
