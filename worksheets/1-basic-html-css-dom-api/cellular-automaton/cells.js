1a2cdb3d16865894b0ce687f080ba9f28d64406f
Yit Chee Chin <yitchee1998@gmail.com>
2018-09-30 22:51:57 +0100

    Complete

--- worksheets/1-basic-html-css-dom-api/cellular-automaton/cells.js
+++ worksheets/1-basic-html-css-dom-api/cellular-automaton/cells.js
@@ -1,37 +1,93 @@
+let cells = [];
+let rows = 50;
+let cols = 101;
+fillArray(cells, rows, cols);
+
 window.onload = function() {
-    var container = document.getElementById('container');
-    document.getElementById("container").style.display = "inline-block";
-    document.getElementById("container").style.whiteSpace = "nowrap"; 
-    document.getElementById("container").style.overflow = "hidden"; 
+    let maxWidth = (cols * 8);
+    let maxHeight = (rows * 8);
+    document.body.style.width  = maxWidth + "px";
+    document.body.style.height  = maxHeight + "px";
 
-    
-    for (let row = 0; row < 50; row++) {
-        for (let col = 0; col < 101; col++) {
-            var para = document.createElement("div");
-            var node = document.createTextNode("");
-            para.classList.add("cell");
-            para.appendChild(node);
-
-            container.appendChild(para);
-        }
-        linebreak = document.createElement("br");
-        container.appendChild(linebreak);
-        linebreak.style.display = 'block';
-        linebreak.style.marginTop = '-11.5px';
+    for (let i = 0; i < cols; i++) {
+        cells[0][i] = Math.round((Math.random()));
+        renderCell(cells[0][i]);
     }
-    var br = document.getElementsByName('br');
     
-    var all = document.getElementsByClassName('cell');
-    
-    for (var i = 0; i < all.length; i++) {
-        all[i].style.display = 'inline-flex';
-        all[i].style.width = '8px';
-        all[i].style.height = '8px';
-        if (i%2 == 0) {
-            all[i].style.background = 'red';
+    for (let i = 1; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            cells[i][j] = createCell(i, j);    
+            renderCell(cells[i][j]);
         }
-        else {
-            all[i].style.background = 'blue';
+    }
+}
+
+function renderCell(active)
+{
+    let cell = document.createElement("div");
+	cell.style.width = "8px";
+	cell.style.height = "8px";
+    cell.style.float = "left";
+    
+	if (active == 1) {
+		cell.style.background = "gray";
+	}
+	else {
+		cell.style.background = "beige";
+	}
+
+	document.body.appendChild(cell);
+}
+
+function createCell(r, c) {
+    let left, right;
+    let mid = c;
+    let prevRow = r-1;
+
+    if (c==0) {
+        left = cols-1;
+        right = c + 1;
+    }
+    else if (c==cols-1) {
+        right = 0;
+        left = c - 1;
+    }
+    else {
+        right = c + 1;
+        left = c - 1;
+    }
+
+    if (cells[prevRow][left]==1 && cells[prevRow][mid]==1 && cells[prevRow][right]==1) {
+        return 0;
+    }
+    else if (cells[prevRow][left]==1 && cells[prevRow][mid]==1 && cells[prevRow][right]==0) {
+        return 0;
+    }
+    else if (cells[prevRow][left]==1 && cells[prevRow][mid]==0 && cells[prevRow][right]==1) {
+        return 1;
+    }
+    else if (cells[prevRow][left]==1 && cells[prevRow][mid]==0 && cells[prevRow][right]==0) {
+        return 1;
+    }
+    else if (cells[prevRow][left]==0 && cells[prevRow][mid]==1 && cells[prevRow][right]==1) {
+        return 1;
+    }
+    else if (cells[prevRow][left]==0 && cells[prevRow][mid]==1 && cells[prevRow][right]==0) {
+        return 1;
+    }
+    else if (cells[prevRow][left]==0 && cells[prevRow][mid]==0 && cells[prevRow][right]==1) {
+        return 0;
+    }
+    else if (cells[prevRow][left]==0 && cells[prevRow][mid]==0 && cells[prevRow][right]==0) {
+        return 0;
+    }
+}
+
+function fillArray(arr, r, c){
+    for (let i = 0; i < r; i++) {
+        arr.push([0])
+        for (let j = 0; j < c; j++) {
+            arr[i][j] = 0;
         }
     }
 }
