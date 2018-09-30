20274f20b7bd9a158acc1b33f813796b7d491a5f
Yit Chee Chin <yitchee1998@gmail.com>
2018-09-30 02:41:51 +0100

    Renders cells

--- worksheets/1-basic-html-css-dom-api/cellular-automaton/cells.js
+++ worksheets/1-basic-html-css-dom-api/cellular-automaton/cells.js
@@ -1,11 +1,37 @@
-let cell = document.createElement("div");
-let node = document.createTextNode("This is new.");
-let body = document.getElementById('body');
+window.onload = function() {
+    var container = document.getElementById('container');
+    document.getElementById("container").style.display = "inline-block";
+    document.getElementById("container").style.whiteSpace = "nowrap"; 
+    document.getElementById("container").style.overflow = "hidden"; 
 
-cell.appendChild(node);
-for (let row = 0; row < 50; row++) {
-    for (let col = 0; col < 101; col++) {
-        body.appendChild(cell);
-        console.log(col);
+    
+    for (let row = 0; row < 50; row++) {
+        for (let col = 0; col < 101; col++) {
+            var para = document.createElement("div");
+            var node = document.createTextNode("");
+            para.classList.add("cell");
+            para.appendChild(node);
+
+            container.appendChild(para);
+        }
+        linebreak = document.createElement("br");
+        container.appendChild(linebreak);
+        linebreak.style.display = 'block';
+        linebreak.style.marginTop = '-11.5px';
+    }
+    var br = document.getElementsByName('br');
+    
+    var all = document.getElementsByClassName('cell');
+    
+    for (var i = 0; i < all.length; i++) {
+        all[i].style.display = 'inline-flex';
+        all[i].style.width = '8px';
+        all[i].style.height = '8px';
+        if (i%2 == 0) {
+            all[i].style.background = 'red';
+        }
+        else {
+            all[i].style.background = 'blue';
+        }
     }
 }
