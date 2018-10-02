36ed02299c7c565bc08b4ff21a5eb8fdb62a3df7
tasyaoc95 <c13365696@mydit.ie>
2018-10-02 11:57:30 +0100

    Changed method of generating row

--- lab1p3.js
+++ lab1p3.js
@@ -5,7 +5,7 @@ var btn1 = document.querySelector('#B1');
 
 
 //btn1.addEventListener('click', removeButton); //when button is clicked remove button
-btn1.addEventListener('click', createGrid); //when button is clicked create grid
+btn1.addEventListener('click', myFunction); //when button is clicked create grid
 
 
 
@@ -20,38 +20,46 @@ function removeButton(btn1) {
 }
 
 //function to create the grid
-function createGrid() {
+/*function createGrid() {
 
 	var divArray = [];//create an array of 100 to be used to create divs
     var temp;
     var var1; //boolean
     
   for (i = 0; i < 101; i++) { //run through array using for loop, 
-  temp = document.createElement('div'); //create div element for each array value
-  temp.className = "divGrid"; //add class name to each div
-  temp.style.border = "thin solid black"; //style of divs
-  temp.style.width = "8px";
-  temp.style.height = "8px";
-  temp.style.position = "relative"
-  temp.innerHTML = divArray[i];
+	  temp = document.createElement('div'); //create div element for each array value
+	  temp.className = "divGrid"; //add class name to each div
+	  temp.style.border = "thin solid black"; //style of divs
+	  temp.style.width = "8px";
+	  temp.style.height = "8px";
+	  temp.style.position = "relative"
+	  temp.innerHTML = divArray[i];*/
+
+
+function myFunction() {
+
+    var row = document.getElementById("myRow");
+
+    for (i = 0; i < 101; i++) { 
+    var x = row.insertCell(0);
+    x.style.border = "thin solid black"; //style of divs
+	  x.style.width = "8px";
+	  x.style.height = "8px";
+
   
   var1 = Math.random(); //random number between 1 and 0
 
   if (var1 > 0.5) {
-  	temp.style.backgroundColor = "yellow";
+  	x.style.backgroundColor = "yellow";
 }
  
  else {
- 	temp.style.backgroundColor = "green"
+ 	x.style.backgroundColor = "green"
  }
-  document.getElementsByTagName('body')[0].appendChild(temp);
 
-
-
-
-  
-  
 }
+  
+ 
 	    
 }
 
