cc957b44c6f2120ccd8467bb984379aa04793e1c
Yit Chee Chin <yitchee1998@gmail.com>
2018-10-06 03:40:00 +0100

    Allowed keyboard input for calculator

--- worksheets/1-basic-html-css-dom-api/calculator-skin/calculator.js
+++ worksheets/1-basic-html-css-dom-api/calculator-skin/calculator.js
@@ -6,10 +6,7 @@ window.onload = function() {
     for(let i = 0; i < buttons_numbers.length; i++) {
         let button = buttons_numbers[i];
         button.onclick = function() {
-            if (checkScreen()) {
-                result.innerText = "";
-            }
-            result.innerText += buttons_numbers[i].innerText;
+            addToScreen(buttons_numbers[i].innerText);
         }
     }
 
@@ -17,31 +14,20 @@ window.onload = function() {
         let button = buttons_operators[i];
 
         button.onclick = function() {
-            if (checkScreen()) {
-                result.innerText = "";
-            }
-            let result_text = result.innerText;
-            let length = result.innerText.length;
-
-            let last_char = result_text.charAt(length - 1);
-
-            if (isNaN(last_char)) {
-                if (last_char == "(" || last_char == ")") {
-                    result.innerText += buttons_operators[i].innerText;
-                }
-                else {
-                    if (buttons_operators[i].innerText == "(" || buttons_operators[i].innerText == ")") {
-                        result.innerText += buttons_operators[i].innerText;
-                    }
-                    else {
-                        result.innerText = result.innerText.slice(0, length-1) + buttons_operators[i].innerText;
-                    }
-                }
-            }
-            else {
-                result.innerText += buttons_operators[i].innerText;
-            }
+            addToScreenOps(buttons_operators[i].innerText);
         }
+    }
+}
+
+// Takes keyboard input
+window.onkeypress = function(e) {
+    let x = e.which || e.keyCode;
+    let key = String.fromCharCode(x);
+    if (isNaN(key) && !isAlpha(key)){
+        addToScreenOps(key);
+    }
+    else {
+        addToScreen(key);
     }
 }
 
@@ -56,6 +42,7 @@ function checkScreen() {
     return false;
 }
 
+// Evaluate inputted string
 function calculate() {
     try {
         result.innerText = eval(result.innerText);
@@ -63,3 +50,62 @@ function calculate() {
         result.innerText = "Error";
     }
 }
+
+// If a number is pressed
+function addToScreen(key) {
+    if (!isAlpha(key)) {
+        if (checkScreen()) {
+            result.innerText = "";
+            result.innerText += key;
+        }
+        else {
+            result.innerText += key;
+        }
+    }    
+}
+
+// If an Operator is pressed
+function addToScreenOps(key) {
+    if (!isAlpha(key)) {
+        if (checkScreen()) {
+            result.innerText = "";
+            result.innerText += key;
+        }
+        else {
+            let result_text = result.innerText;
+            let length = result.innerText.length;
+
+            let last_char = result_text.charAt(length - 1);
+
+            if (isNaN(last_char)) {
+                if (last_char == "(" || last_char == ")") {
+                    result.innerText += key;
+                }
+                else {
+                    if (key == "(" || key == ")") {
+                        result.innerText += key;
+                    }
+                    else {
+                        result.innerText = result.innerText.slice(0, length-1) + key;
+                    }
+                }
+            }
+            else {
+                result.innerText += key;
+            }
+        }
+    }    
+}
+
+// Check if string is a letter
+function isAlpha(str) {
+    var code, i, len;
+  
+    for (i = 0, len = str.length; i < len; i++) {
+        code = str.charCodeAt(i);
+        if (!(code > 64 && code < 91) && !(code > 96 && code < 123)) {
+            return false;
+        }
+    }
+    return true;
+  };
