b562576f8df8594e6619369cb3658af57c4dc8a5
david-p-burton <david.p.burton@gmail.com>
2018-10-17 18:35:02 +0100

    Work so far

--- worksheets/2-Async-Programming/question2/q2.js
+++ worksheets/2-Async-Programming/question2/q2.js
@@ -14,38 +14,60 @@ function main()
 function objectsNameCityZip()
 {
 	//list objects with a Username, City and Zipcode
-	fetch('https://jsonplaceholder.typicode.com/users/')
-		.then(response => response.json())
-		.then(json => console.log(json))
-		;
+	const apiURL = 'https://jsonplaceholder.typicode.com/users/';
 
-	// const foo = fetch('https://jsonplaceholder.typicode.com/users/');
-	// foo
-	// 	.then((response) => {
-	// 		if(response.ok) {
-	// 			var useable = response.json();
-	// 			useable = JSON.stringify(useable);
-	// 			console.log(useable);
+	fetch(apiURL)
+		.then(response => {
 
-	// 		} else {
-	// 			//something else
-	// 		}
-	// 	})
-	// 	.catch((error) => {
-	// 		console.log('Error + ' + error.message);
-	// 	});
+		 return response.json();
 
-	
-	// var bar = foo.filter(response => response.json.Username)
-	// 			.map(ans => console.log(ans))
-	// 			 ;
+		})
+		.then(data => {
+
+			var names = data.map(nameFunc);
+
+			console.log(names);
+
+		})
+		.catch((error) => {
+			console.log('Error: ' + error.message);
+		});
+}
+
+function nameFunc(obj)
+{
+	let ans = obj.username + " " + obj.address.city + " " + obj.address.zipcode;
+	return ans;
 }
 
 //part 2
 function twoOrFive()
 {
 	//Show users having Zipcodes that start with 2 or 5
+	const apiURL = 'https://jsonplaceholder.typicode.com/users/';
+	fetch(apiURL)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			console.log(data.reduce(addition))
+		})
+		.catch((error) => {
+			console.log('Error: ' + error.message);
+		});
+}
 
+function addition(accum, currentVal)
+{
+	return accum + currentVal;
+	// if(obj.address.zipcode.charAt(0) == 2 || obj.address.zipcode.charAt(0) == 5)
+	// {
+	// 	return accum + currentVal;
+	// }
+	// else
+	// {
+	// 	return accum;
+	// }
 }
 
 //part 3
