1b88d9b640d1e0474c4eb5b7477b33241f632006
Eoghan Quirke <eoghanquirke@gmail.com>
2018-10-18 15:32:30 +0100

    Added keyboard functionality to calculator

--- worksheets/2-Asynchronous-Programming/Calculator/calculator.js
+++ worksheets/2-Asynchronous-Programming/Calculator/calculator.js
@@ -20,3 +20,9 @@ function evalScreen(){
     expression = eval(expression);
     display.value = expression;
 }
+
+document.addEventListener('keypress', function(event) {
+    "0123456789-+/*0.".indexOf(event.key) != -1? append(event.key): null;
+    event.key == 'c' || event.key == 'C' ? clearScreen() : null;
+    event.key == '='? evalScreen(): null;
+});
