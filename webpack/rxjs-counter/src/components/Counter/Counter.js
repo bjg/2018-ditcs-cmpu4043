7c68071010227a5946badf1f215f3567a88c9021
Brian Gillespie <brian.j.gillespie@gmail.com>
2018-10-30 16:10:57 +0000

    Improve counter initialisation logic

--- webpack/rxjs-counter/src/components/Counter/Counter.js
+++ webpack/rxjs-counter/src/components/Counter/Counter.js
@@ -2,8 +2,8 @@ import { fromEvent, merge, pipe, subscribe } from "rxjs";
 import { mapTo, scan, startWith } from "rxjs/operators";
 import "./Counter.css";
 
-function Counter() {
-    const display = Display(0);
+function Counter({ initialValue }) {
+    const display = Display(initialValue);
     const incButton = Button("⬆️");
     const decButton = Button("⬇️");
     const rstButton = Button("⏹");
@@ -16,15 +16,21 @@ function Counter() {
     return container;
 }
 
+/*
+ * Solution:
+ * 1. Map idividual button click events to literal functions using the mapto() operator
+ * 2. Initialize the stream with a counter value with the startWith() operator
+ * 3. Update the counter with appropriate function (from step 1)
+ * 4. Subscribe to the stream and update the DOM for each event
+ */
 function run(display, incButton, decButton, rstButton) {
-    const initialValue = 0;
     merge(
         fromEvent(incButton, "click").pipe(mapTo(counter => ({ value: counter.value + 1 }))),
         fromEvent(decButton, "click").pipe(mapTo(counter => ({ value: counter.value - 1 }))),
         fromEvent(rstButton, "click").pipe(mapTo(counter => ({ value: 0 })))
     )
         .pipe(
-            startWith({ value: initialValue }),
+            startWith({ value: Number(display.value) }),
             scan((acc, update) => update(acc))
         )
         .subscribe(counter => {
@@ -34,7 +40,7 @@ function run(display, incButton, decButton, rstButton) {
 
 function Display(initialValue) {
     const display = document.createElement("input");
-    display.placeholder = initialValue;
+    display.value = initialValue;
     display.readonly = "readonly";
     display.style.width = "80px";
     display.style.textAlign = "right";
