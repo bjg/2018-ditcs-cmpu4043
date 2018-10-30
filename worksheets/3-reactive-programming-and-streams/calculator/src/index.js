1cf8564f589f19dda7b02e2140d8c3099a7f1dab
DefinitePurple <danieljfitzpatrick3696@gmail.com>
2018-10-30 16:24:49 +0000

    Removed console.log

--- worksheets/3-reactive-programming-and-streams/calculator/src/index.js
+++ worksheets/3-reactive-programming-and-streams/calculator/src/index.js
@@ -3,5 +3,4 @@ import {Calculator} from "./components/calculator/init"
 import {Observable} from 'rxjs/Rx';
 
 const calculator = Calculator();
-console.log(calculator);
 document.getElementsByTagName("BODY")[0].appendChild(calculator);
