daef6e991d12fbe9445aa63f46ab1d5c41a04630
insomniac807 <c15742551@mydit.ie>
2018-11-01 23:58:43 +0000

    analogue hands added. next add split and clean up ui

--- worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/src/js/app.js
+++ worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/src/js/app.js
@@ -8,15 +8,30 @@ canvas.height = window.innerHeight - (parseFloat(header.style.height)+50);
 
 /** ###################################    WATCH FACE LOGIC ################################################*/
 let context = canvas.getContext('2d');
-var watch_face_x = canvas.width /2;
-var watch_face_y = canvas.height / 2;
+var analogue_display_x = canvas.width /2;
+var analogue_display_y = canvas.height / 2;
 const radius = 200;
 const start_angle = 0;
 const end_angle = 2 * Math.PI;
 const counter_clockwise = false;
 
+function analogue_display_hand(x1, y1, angle, length, width, colour) {
+    let x2 = Math.cos(angle);
+    let y2 = Math.sin(angle); 
+    context.translate(x1, y1);
+    context.rotate(angle);
+    context.beginPath();
+    context.moveTo(0,0);
+    context.lineTo(x2, y2-length);
+    context.line_width = width;
+    context.strokeStyle = colour;
+    context.stroke();
+    context.rotate(-angle);
+    context.translate(-x1, -y1);
+}
+
 //starting at top center of watch face perimeter, roataes about an angle for a given number of iterations, increasing the size of the angle by a factor of iteration index, for every iteration, drawing the interval lines at their appropriate positions and angles relative to the center of the watch face
-function watch_face_intervals(angle_of_rotation, num_iterations, line_length, line_width, colour, center_x, center_y) {
+function analogue_display_intervals(angle_of_rotation, num_iterations, line_length, line_width, colour, center_x, center_y) {
     context.translate(center_x, center_y);
     for(let i= 1; i <= num_iterations; i++){
         let angle = i * angle_of_rotation;
@@ -34,21 +49,25 @@ function watch_face_intervals(angle_of_rotation, num_iterations, line_length, li
         context.rotate(-angle);
     }
     context.translate(-center_x,-center_y);
-}//end watch_face_intervals()
+}//end analogue_display_intervals()
 
 //draws a circular perimeter for a given radius and draws interval markers for minutes, seconds and half seconds
-function watch_face(x,y,radius,start_angle,end_angle,counter_clockwise) {
+function analogue_display(x,y,radius,start_angle,end_angle,counter_clockwise) {
     context.beginPath();
     context.arc(x, y, radius, start_angle, end_angle, counter_clockwise);
     context.lineWidth = 10;
     context.strokeStyle = 'black';
     context.stroke();  
-    watch_face_intervals(Math.PI / 60, 120, 6, 1, 'red', x, y); //half second
-    watch_face_intervals(Math.PI / 30, 60, 10, 3, 'black', x, y); //second
-    watch_face_intervals(Math.PI / 6, 12, 20, 5, 'black', x, y); //minute
+    analogue_display_intervals(Math.PI / 60, 120, 6, 1, 'red', x, y); //half second
+    analogue_display_intervals(Math.PI / 30, 60, 10, 3, 'black', x, y); //second
+    analogue_display_intervals(Math.PI / 6, 12, 20, 5, 'black', x, y); //minute
+    let tenth_angle = (Math.PI / 60)*tenth_seconds%121;
+    let sec_angle = (Math.PI / 30)*get_seconds();
+    let min_angle = (Math.PI / 30)*get_mins();
+    analogue_display_hand(x,y, tenth_angle, radius, 1, 'gold');
+    analogue_display_hand(x,y, sec_angle, radius, 2, 'black');
+    analogue_display_hand(x,y, min_angle, radius*0.9, 3, 'red');
 }
-//draw the watch face
-watch_face(watch_face_x, watch_face_y, radius, start_angle, end_angle, counter_clockwise);
 
 
 /**#####################################################  BUTTONS AND DIGITAL DISPLAY LOGIC ########################################## */
@@ -56,54 +75,51 @@ const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const split = document.querySelector('#split');
 const reset = document.querySelector('#reset');
-let mins = 0;
-let seconds = 0;
-let tenths = 0;
-var frame;
+let tenth_seconds = 0;
 
-const tick = (value, max) => (value+1) % max;
+const tick = () => tenth_seconds+=1;
+const get_tenths = () => tenth_seconds % 11;
+const get_seconds = () => Math.floor((tenth_seconds / 10) % 60);
+const get_mins = () => Math.floor((tenth_seconds / 600) % 61);
+const display_tenths = () => get_tenths() < 10? "0"+get_tenths().toString() : get_tenths().toString();
+const display_seconds = () => get_seconds() < 10? "0"+get_seconds().toString() : get_seconds().toString();
+const display_mins = () => get_mins() < 10? "0"+get_mins().toString() : get_mins().toString();
 
-const digital_watch = () => {
-    let mins_text = mins < 10 ? "0"+mins.toString() : mins.toString();
-    let secs_text = seconds < 10 ? "0"+seconds.toString() : seconds.toString();
-    let tnth_text = tenths < 10 ? "0"+tenths.toString() : tenths.toString();
-    let digital_watch_x = 50;
-    let digital_watch_y = 300;
-    let watch_val = mins_text+":"+secs_text+":"+tnth_text;
+const digital_display = () => {
+    let digital_display_x = 50;
+    let digital_display_y = 300;
+    let watch_val = display_mins()+":"+display_seconds()+":"+display_tenths();
     context.font = '80px serif';
     context.strokeStyle = 'red';
-    context.strokeText(watch_val, digital_watch_x, digital_watch_y, 1000, 1000);
+    context.strokeText(watch_val, digital_display_x, digital_display_y, 1000, 1000);
 }
 
-digital_watch();
-
-const animate_watch = () => {
-    frame = window.requestAnimationFrame(animate_watch);
+const draw_frame = () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    mins = tick(mins, 60);
-    digital_watch();
-    watch_face(watch_face_x, watch_face_y, radius, start_angle, end_angle, counter_clockwise);
-    
+    digital_display();
+    analogue_display(analogue_display_x, analogue_display_y, radius, start_angle, end_angle, counter_clockwise);
 }
 
-const stop_watch = () => {
-    window.cancelAnimationFrame(frame);
-    return;
-}
-
+let running = false;
+const animate_watch = () => { if(running){ tick(); draw_frame();} }
+const start_watch = () => running = true;
+const stop_watch = () => running = false;
 const reset_watch = () => {
-    mins = 0;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    digital_watch();
-    watch_face(watch_face_x, watch_face_y, radius, start_angle, end_angle, counter_clockwise);
+    tenth_seconds = 0;
+    draw_frame();
 }
 
-let start$ = Observable.fromEvent(start, 'click').mapTo(animate_watch);
-let stop$ = Observable.fromEvent(stop, 'click').mapTo(stop_watch);
-let reset$ = Observable.fromEvent(reset, 'click').mapTo(reset_watch);
+//init frame
+draw_frame();
+//create event and time interval streams
+const observables$ = Observable.merge(
+    Observable.fromEvent(start, 'click').map(e => start_watch()),
+    Observable.fromEvent(stop, 'click').map(e => stop_watch()),
+    Observable.fromEvent(reset, 'click').map(e => reset_watch()),
+    Observable.interval(100).map(e => animate_watch())
+);
 
-start$.subscribe();
-stop$.subscribe();
-reset$.subscribe();
+observables$
+    .subscribe();
 
 
