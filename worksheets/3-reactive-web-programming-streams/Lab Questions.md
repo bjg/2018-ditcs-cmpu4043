47b53d8c6f38d5f0384d1d07b23a6063b9471503
Ciaran-OBrien <c15765215@mydit.ie>
2018-11-01 21:44:52 +0000

    Commenting Lab Solution along wih review Questions

---
+++ worksheets/3-reactive-web-programming-streams/Lab Questions.md
@@ -0,0 +1,24 @@
+# Lecture Review Questions 
+
+## Excercise 1
+
+Explain what is meant by the stream abstraction. 
+
+
+Streams can be simplified down into 4 fundemental types;
+- Transform
+- Duplex
+- Writable
+- Readale
+
+What is the relationship between streams and the observer pattern?  
+
+What are streams useful for modeling and when might you use them in Rich Web development?
+
+## Excercise 2
+
+Assume that you are building an interface to an API in your Rich Web App. Describe in detail how you could use the RxJS library to handle asynchronous network responses to API requests. 
+
+In your opinion, what are the benefits to using a streams library for networking over, say, promises? 
+
+And what do you think are the downsides?
--- worksheets/3-reactive-web-programming-streams/rx-stopwatch/src/index.js
+++ worksheets/3-reactive-web-programming-streams/rx-stopwatch/src/index.js
@@ -1,147 +1,100 @@
 import "./style.css";
-import {Observable, fromEvent, merge } from 'rxjs';
+import {Observable, fromEvent, merge, interval } from 'rxjs';
 import Rx from 'rxjs/Rx';
 
+// Get all the DOM elements required
+const canvas = document.getElementById('canvas');
+const timer = document.getElementById('timer');
+const timerList = document.getElementById('time-container');
 
+let source = interval(100).timeInterval();
 
-/*-------------------------------------------*\
-    script.js
-    Javascript in response to Lab Question 2.1
+let started = false;
+let time = 0;
 
-    Author: Ciarán O'Brien
-    Lecture: Brian Gillespie
-    Student Number: C15765215
-    Date: 20/09/18
-\*-------------------------------------------*/
-let calculating = false
-let negative = false
+let elem = document.createElement("img")
+elem.src = 'watchFace.png';
+elem.class = "face"
+document.getElementById("watchFace").appendChild(elem)
 
-// Function that handles and preform the correct funciton for each button pressed
-// Error Checking involves insuring no doulbe arithmetic symbols are added to the input
-function btn_handler(btn_pressed) {
-	console.log("btn_handler sees",btn_pressed)
-    let screenValue = document.getElementById("calculation").value
-    // Check that the value entered is a numeric value 
-    if (Number.isInteger(parseInt(btn_pressed))) {
-        calculating = false
-        addToScreen(btn_pressed)
+// Main subcriber FXN
+const subscription = source.subscribe(
+  x => {
+    let seconds = Math.floor((time / 10) % 60)
+    let minutes = Math.floor(time / 600)
+    if(!started) return;
+    time++;
+    // Formatting for the second overflow, going from 09 -> 10 seconds
+    if(minutes > 9){
+        timer.innerHTML = minutes + ":" + "0" + seconds + ":" + (time % 10) + "0";
     }
-    // If it's not an interger number, enter the switch cases
-    else {
-        switch (btn_pressed) { // Swtich about whichever button has been pressed/entered
-            case "c":
-            case "C": // Case to clear the screen
-                document.getElementById("calculation").value = ''
-                break
-            case "+": // Addition case
-                if (!calculating) {
-                    addToScreen(btn_pressed)
-                    calculating = true
-                    break
-                }
-                break
-            case "−": //Subtraction case
-                if (!calculating) {
-                    calculating = true
-                    negative = true
-                    addToScreen(btn_pressed)
-                    break
-                }
-                break;
-            case "×": // Multiplication case
-            	console.log("hit")
-                if (!calculating) {
-                    calculating = true
-                    addToScreen(btn_pressed)
-                    break
-                }
-                break;
-            case "÷": // Division case
-                if (!calculating) {
-                    calculating = true
-                    addToScreen(btn_pressed)
-                    break
-                }
-                break
-            case "(": // Left bracket case
-                addToScreen(btn_pressed)
-                break
-            case ")": // Right bracket case
-                addToScreen(btn_pressed)
-                break
-            case ".": // Decimal point case
-                addToScreen(btn_pressed)
-                break
-            case "=": // Evaluate equation case
-                      // We must try execute the eval() fxn
-                      // If there's an error in execusion, it'll be caught
-                try {
-                    negative = false
-                    calculating = false
-                    document.getElementById("calculation").value = calculate(document.getElementById("calculation").value)
-                    break
-                } catch (e) {
-                    if (e instanceof SyntaxError) {
-                        alert(e.message)
-                        break
-                    }
-                }
-                break
-            case "±": // Negation case
-                pushToScreen()
-                break
-            default: // Default just in case
-            	console.log("Broken")
-                break
-
-        }
+    if(seconds> 9){
+        timer.innerHTML = "0" + minutes + ":" + seconds + ":" + (time % 10) + "0";    
+    }else{
+        timer.innerHTML = "0" + minutes + ":" + "0" + seconds + ":" + (time % 10) + "0";    
     }
-}
+    draw(time);
+  });
 
-function addToScreen(value) { // FXN to update the calculators screen 
-    document.getElementById("calculation").value += value
-}
-// FXN to push the negation to the front of the current value
-// Does nothing if the system is currently exspecting a number, i.e. not an arithmic value
-// Error checking evolved is checking if the last value on screen is already negative
-// Mix of splice() and regex in order to change the last value, exploring different methods to do so  
-function pushToScreen() { 
-    if (calculating) {
+// Handling the start button click event
+fromEvent(document.getElementById('start-button'), 'click')
+  .subscribe(e => {
+    started = true;
+  });
+
+// Handling the stop button click event
+fromEvent(document.getElementById('stop-button'), 'click')
+  .subscribe(e => {
+    started = false;
+  });
+
+// Handling the stop button click event
+fromEvent(document.getElementById('split-button'), 'click')
+  .subscribe(e => {
+    timerList.innerHTML += timer.innerHTML + "<br/>";
+  });
+
+// Handling the reset button click event
+fromEvent(document.getElementById('reset-button'), 'click')
+  .subscribe(e => {
+    started = false;
+    time = 0;
+    draw(time);
+    timer.innerHTML = "00:00:00";
+    splitsList.innerHTML = "";
+  });
+
+// Draw each arm of the clock, and move them appropriately, relative to timeInterval
+const draw = (time) => {
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    // Clear the path and the area about which the minute hands will be deplayed 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
     
-    } else {
-        let toNegateLocation = document.getElementById("calculation").value.length - 1
-        let toNegate = document.getElementById("calculation").value[toNegateLocation]
-        let val = document.getElementById("calculation").value
-        if (negative) {
-            valHolder = [...val]
-            val = valHolder.splice(toNegateLocation - 1, 2, Math.abs(toNegate))
-            val = valHolder.join('')
-            document.getElementById("calculation").value = val
-            negative = false;
-        } else {
-            document.getElementById("calculation").value = document.getElementById("calculation").value.replace(/.$/, -Math.abs(toNegate))
-            negative = true;
-        }
-    }
+    const watchSize = 96;
+    const contentSize = 0.92;
+
+    ctx.strokeStyle = "Gray";
+    
+
+    // Longer hand (minute), each minute goes one step
+    // The angle is found by dividing down to each minute
+    // 0.25 is to ensure that the hnad starts at 0
+    let angle = (time / 600 / 60 - 0.25) * (Math.PI * 2);
+    let armLength = watchSize * 0.5;
+    ctx.moveTo(watchSize, watchSize);
+    ctx.lineTo(watchSize + armLength * Math.cos(angle), watchSize + armLength * Math.sin(angle));
+
+    // Shorter hand (second), each second goes one step
+    angle = (time / 10 / 60 - 0.25) * (Math.PI * 2);
+    armLength = watchSize * 0.8;
+    ctx.moveTo(watchSize, watchSize);
+    ctx.lineTo(watchSize + armLength * Math.cos(angle), watchSize + armLength * Math.sin(angle));
+
+    ctx.stroke();
+  }
 }
 
-// FXN simply evaluates the current input to the calculator
-// Regex is used to replace multiply and divide values
-function calculate(toEval) { // Evaluate the current equation 
-    return eval(toEval.replace(/×/, '*').replace(/÷/, '/').replace(/−/,'-'))
-}
-
-let keyUp = fromEvent(document,'keyup')
-
-keyUp.subscribe(function(value) {
-    btn_handler(value.key)
-});
-
-const toggleButton = document.querySelector('.leftParam') 
-
-let buttons = document.getElementsByTagName('button')
-let clicked = fromEvent(buttons,'click')
-clicked.subscribe((value) => {
-	console.log(value["target"].innerHTML)
-	btn_handler(value["target"].innerHTML)
-})
+draw();
