d8c84abd7b3060afa00219ae37a1c8c72b0a9532
insomniac807 <c15742551@mydit.ie>
2018-11-01 04:55:08 +0000

    refactored draw clock face code

--- worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/app.js
+++ worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/app.js
@@ -1,26 +1,24 @@
 let canvas = document.querySelector('canvas');
 let header = document.querySelector('#heading');
 header.style.height = "36px";
-console.log(header);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - (parseFloat(header.style.height)+50); 
-
 let context = canvas.getContext('2d');
 var x = canvas.width /2;
 var y = canvas.height / 2;
 var radius = 200;
-var startAngle = 0;
-var endAngle = 2 * Math.PI;
+var start_angle = 0;
+var end_angle = 2 * Math.PI;
 var counterClockwise = false;
 
 
-function clock_face_intervals(angle_of_rotation, line_length, line_width, colour) {
+function clock_face_intervals(angle_of_rotation, num_iterations, line_length, line_width, colour) {
     context.translate(x,y);
-    for(i= 1; i < 120; i++){
-        let ang = i * angle_of_rotation;
-        let x1 = Math.cos(ang);
-        let y1 = Math.sin(ang);
-        context.rotate(ang);
+    for(i= 1; i < num_iterations; i++){
+        let angle = i * angle_of_rotation;
+        let x1 = Math.cos(angle);
+        let y1 = Math.sin(angle);
+        context.rotate(angle);
         context.translate(0, -radius*0.85);
         context.beginPath();
         context.moveTo(x1,y1-line_length);
@@ -29,7 +27,7 @@ function clock_face_intervals(angle_of_rotation, line_length, line_width, colour
         context.strokeStyle = colour;
         context.stroke();
         context.translate(0, radius*0.85);
-        context.rotate(-ang);
+        context.rotate(-angle);
     }
     context.translate(-x,-y);
 
@@ -38,14 +36,13 @@ function clock_face_intervals(angle_of_rotation, line_length, line_width, colour
 
 function clockFace() {
     context.beginPath();
-    context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+    context.arc(x, y, radius, start_angle, end_angle, counterClockwise);
     context.lineWidth = 10;
-    // line color
     context.strokeStyle = 'black';
-    context.stroke();   
-    clock_face_intervals(Math.PI / 60, 6, 1, 'red');   
-    clock_face_intervals(Math.PI / 30, 10, 3, 'black');   
-    clock_face_intervals(Math.PI / 6, 20, 5, 'black');
+    context.stroke();  
+    clock_face_intervals(Math.PI / 60, 120, 6, 1, 'red'); //half second
+    clock_face_intervals(Math.PI / 30, 60, 10, 3, 'black'); //second
+    clock_face_intervals(Math.PI / 6, 24, 20, 5, 'black'); //minute
 }
 
 clockFace();
