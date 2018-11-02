f2683cac75f5b8ed97f8d2d42095c874bb915d85
insomniac807 <c15742551@mydit.ie>
2018-11-02 21:06:27 +0000

    commit

--- worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/src/js/app.js
+++ worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/src/js/app.js
@@ -1,129 +1,34 @@
 import {Observable} from 'rxjs/Rx';
-import digitalClockFont from '../assets/digital-7.ttf';
-
-let canvas = document.querySelector('canvas');
-let header = document.querySelector('#heading');
-header.style.height = "36px";
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - (parseFloat(header.style.height)+50); 
-
-/** ###################################    WATCH FACE LOGIC ################################################*/
-let context = canvas.getContext('2d');
-var analogue_display_x = canvas.width /2;
-var analogue_display_y = canvas.height / 2;
-const radius = 200;
-const start_angle = 0;
-const end_angle = 2 * Math.PI;
-const counter_clockwise = false;
-
-function analogue_display_hand(x1, y1, angle, length, width, colour) {
-    let x2 = Math.cos(angle);
-    let y2 = Math.sin(angle); 
-    context.translate(x1, y1);
-    context.rotate(angle);
-    context.beginPath();
-    context.moveTo(0,0);
-    context.lineTo(x2, y2-length);
-    context.line_width = width;
-    context.strokeStyle = colour;
-    context.stroke();
-    context.rotate(-angle);
-    context.translate(-x1, -y1);
-}
-
-//starting at top center of watch face perimeter, roataes about an angle for a given number of iterations, increasing the size of the angle by a factor of iteration index, for every iteration, drawing the interval lines at their appropriate positions and angles relative to the center of the watch face
-function analogue_display_intervals(angle_of_rotation, num_iterations, line_length, line_width, colour, center_x, center_y) {
-    context.translate(center_x, center_y);
-    for(let i= 1; i <= num_iterations; i++){
-        let angle = i * angle_of_rotation;
-        let x1 = Math.cos(angle);
-        let y1 = Math.sin(angle);
-        context.rotate(angle);
-        context.translate(0, -radius*0.85);
-        context.beginPath();
-        context.moveTo(x1,y1-line_length);
-        context.lineTo(x1,y1+line_length/2);
-        context.lineWidth = line_width;
-        context.strokeStyle = colour;
-        context.stroke();
-        context.translate(0, radius*0.85);
-        context.rotate(-angle);
-    }
-    context.translate(-center_x,-center_y);
-}//end analogue_display_intervals()
-
-//draws a circular perimeter for a given radius and draws interval markers for minutes, seconds and half seconds
-function analogue_display(x,y,radius,start_angle,end_angle,counter_clockwise) {
-    context.beginPath();
-    context.arc(x, y, radius, start_angle, end_angle, counter_clockwise);
-    context.lineWidth = 10;
-    context.strokeStyle = '#FF9F00';
-    context.stroke(); 
-    analogue_display_intervals(Math.PI / 60, 120, 6, 1, 'red', x, y); //half second
-    analogue_display_intervals(Math.PI / 30, 60, 10, 3, '#E0A800', x, y); //second
-    analogue_display_intervals(Math.PI / 6, 12, 20, 5, '#AC6B00', x, y); //minute
-    let tenth_angle = (Math.PI / 60)*tenth_seconds%121;
-    let sec_angle = (Math.PI / 30)*get_seconds();
-    let min_angle = (Math.PI / 30)*get_mins();
-    analogue_display_hand(x,y, tenth_angle, radius, 1, '#985B00');
-    analogue_display_hand(x,y, sec_angle, radius, 2, '#DAB500');
-    analogue_display_hand(x,y, min_angle, radius*0.9, 3, '#AC6B00'); 
-    context.arc(x,y,10,start_angle,end_angle);
-    context.fillStyle = '#B87900';
-    context.fill();
-}
-
-
-/**#####################################################  BUTTONS AND DIGITAL DISPLAY LOGIC ########################################## */
+import {Stopwatch} from './watch_face.js';
+import css from '../css/style.css';
+//create variables
+const canvas = document.querySelector('canvas');
+const header = document.querySelector('#heading');
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const split = document.querySelector('#split');
 const reset = document.querySelector('#reset');
-let tenth_seconds = 0;
-
-const tick = () => tenth_seconds+=1;
-const get_tenths = () => tenth_seconds % 10;
-const get_seconds = () => Math.floor((tenth_seconds / 10) % 60);
-const get_mins = () => Math.floor((tenth_seconds / 600) % 61);
-const display_tenths = () => get_tenths() < 10? "0"+get_tenths().toString() : get_tenths().toString();
-const display_seconds = () => get_seconds() < 10? "0"+get_seconds().toString() : get_seconds().toString();
-const display_mins = () => get_mins() < 10? "0"+get_mins().toString() : get_mins().toString();
-
-const digital_display = () => {
-    let digital_display_x = 50;
-    let digital_display_y = 300;
-    let watch_val = display_mins()+":"+display_seconds()+":"+display_tenths();
-    context.font = '80px Arial';
-    context.strokeStyle = '#800E0E';
-    context.strokeText(watch_val, digital_display_x, digital_display_y, 1000, 1000);
-}
-
-const draw_frame = () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    digital_display();
-    analogue_display(analogue_display_x, analogue_display_y, radius, start_angle, end_angle, counter_clockwise);
-}
-
-let running = false;
-const animate_watch = () => { if(running){ tick(); draw_frame();} }
-const start_watch = () => running = true;
-const stop_watch = () => running = false;
-const reset_watch = () => {
-    tenth_seconds = 0;
-    draw_frame();
-}
-
+const radius = 200;
+//set canvas width and height
+header.style.height = "36px";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - (parseFloat(header.style.height)+50); 
+const analogue_display_x = canvas.width /2;
+const analogue_display_y = canvas.height / 2;
+const digital_display_x = 100;
+const digital_display_y = 350;
+const splits_x = 1100;
+const splits_y = 200;
+const context = canvas.getContext('2d');
+const stopwatch = new Stopwatch(context, canvas.width, canvas.height, analogue_display_x, analogue_display_y, digital_display_x, digital_display_y, splits_x, splits_y, radius);
 //init frame
-draw_frame();
+stopwatch.draw_frame();
 //create event and time interval streams
 const observables$ = Observable.merge(
-    Observable.fromEvent(start, 'click').map(e => start_watch()),
-    Observable.fromEvent(stop, 'click').map(e => stop_watch()),
-    Observable.fromEvent(reset, 'click').map(e => reset_watch()),
-    Observable.interval(100).map(e => animate_watch())
+    Observable.fromEvent(start, 'click').map(e => stopwatch.start_watch()),
+    Observable.fromEvent(stop, 'click').map(e => stopwatch.stop_watch()),
+    Observable.fromEvent(split, 'click').map(e => stopwatch.add_split()),
+    Observable.fromEvent(reset, 'click').map(e => stopwatch.reset_watch()),
+    Observable.interval(100).map(e => stopwatch.animate_watch())
 );
-
-observables$
-    .subscribe();
-
-
+observables$.subscribe();
--- worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/src/js/watch_face.js
+++ worksheets/3-Reactive-Web-Programming-and-Streams/stopwatch/src/js/watch_face.js
@@ -1,34 +1,132 @@
-function clock_face_intervals(angle_of_rotation, num_iterations, line_length, line_width, colour) {
-    context.translate(x,y);
-    for(i= 1; i <= num_iterations; i++){
-        let angle = i * angle_of_rotation;
-        let x1 = Math.cos(angle);
-        let y1 = Math.sin(angle);
-        context.rotate(angle);
-        context.translate(0, -radius*0.85);
-        context.beginPath();
-        context.moveTo(x1,y1-line_length);
-        context.lineTo(x1,y1+line_length/2);
-        context.lineWidth = line_width;
-        context.strokeStyle = colour;
-        context.stroke();
-        context.translate(0, radius*0.85);
-        context.rotate(-angle);
+export class Stopwatch {
+
+    constructor(context, canvas_width, canvas_height, ax, ay, dx, dy, sx, sy, radius) {
+        this.context = context;
+        this.analogue_x = ax;
+        this.analogue_y = ay; 
+        this.digital_x = dx;
+        this.digital_y = dy;
+        this.splits_x = sx;
+        this.splits_y = sy;
+        this.radius = radius;
+        this.canvas_width = canvas_width;
+        this.canvas_height = canvas_height;
+        this.start_angle = 0;
+        this.end_angle = Math.PI *2;
+        this.counter_clockwise = false;
+        this.tenth_seconds = 0;
+        this.running = false;
+        this.splits = [];
     }
-    context.translate(-x,-y);
 
-}
+    tick(){ return this.tenth_seconds+=1; }
+    get_tenths(){ return this.tenth_seconds % 10; }
+    get_seconds(){ return Math.floor((this.tenth_seconds / 10) % 60); }
+    get_mins(){ return Math.floor((this.tenth_seconds / 600) % 61); }
+    display_tenths(){ return "0"+this.get_tenths().toString(); }
+    display_seconds(){ return this.get_seconds() < 10? "0"+this.get_seconds().toString() : this.get_seconds().toString(); }
+    display_mins(){ return this.get_mins() < 10? "0"+this.get_mins().toString() : this.get_mins().toString(); }
+    display_time(){ return this.display_mins()+":"+this.display_seconds()+":"+this.display_tenths(); }
 
+    analogue_display_hand(x1, y1, angle, length, width, colour) {
+        this.context.translate(x1, y1);
+        let x2 = Math.cos(angle);
+        let y2 = Math.sin(angle); 
+        this.context.rotate(angle);
+        this.context.beginPath();
+        this.context.moveTo(0,0);
+        this.context.lineTo(x2, y2-length);
+        this.context.line_width = width;
+        this.context.strokeStyle = colour;
+        this.context.stroke();
+        this.context.rotate(-angle);
+        this.context.translate(-x1,-y1);
+    }
+    
+    //starting at top center of watch face perimeter, roataes about an angle for a given number of iterations, increasing the size of the angle by a factor of iteration index, for every iteration, drawing the interval lines at their appropriate positions and angles relative to the center of the watch face
+    analogue_display_intervals(angle_of_rotation, num_iterations, line_length, line_width, colour, center_x, center_y) {
+        this.context.translate(center_x, center_y);
+        for(let i= 1; i <= num_iterations; i++){
+            let angle = i * angle_of_rotation;
+            let x1 = Math.cos(angle);
+            let y1 = Math.sin(angle);
+            this.context.rotate(angle);
+            this.context.translate(0, -this.radius*0.85);
+            this.context.beginPath();
+            this.context.moveTo(x1,y1-line_length);
+            this.context.lineTo(x1,y1+line_length/2);
+            this.context.lineWidth = line_width;
+            this.context.strokeStyle = colour;
+            this.context.stroke();
+            this.context.translate(0, this.radius*0.85);
+            this.context.rotate(-angle);
+        }
+        this.context.translate(-center_x,-center_y);
+    }//end analogue_display_intervals()
+    
 
-function clock_face(x,y,radius,start_angle,end_angle,counter_clockwise, context) {
-    context.beginPath();
-    context.arc(x, y, radius, start_angle, end_angle, counter_clockwise);
-    context.lineWidth = 10;
-    context.strokeStyle = 'black';
-    context.stroke();  
-    clock_face_intervals(Math.PI / 60, 120, 6, 1, 'red', context); //half second
-    clock_face_intervals(Math.PI / 30, 60, 10, 3, 'black', context); //second
-    clock_face_intervals(Math.PI / 6, 12, 20, 5, 'black', context); //minute
-}
+    //draws a circular perimeter for a given radius and draws interval markers for minutes, seconds and half seconds
+    analogue_display() {
+        this.context.beginPath();
+        this.context.arc(this.analogue_x, this.analogue_y, this.radius, this.start_angle, this.end_angle, this.counter_clockwise);
+        this.context.lineWidth = 10;
+        this.context.strokeStyle = '#FF9F00';
+        this.context.stroke(); 
+        this.analogue_display_intervals(Math.PI / 60, 120, 6, 1, 'red', this.analogue_x, this.analogue_y); //half second
+        this.analogue_display_intervals(Math.PI / 30, 60, 10, 3, '#E0A800', this.analogue_x, this.analogue_y); //second
+        this.analogue_display_intervals(Math.PI / 6, 12, 20, 5, '#AC6B00', this.analogue_x, this.analogue_y); //minute
+        let tenth_angle = (Math.PI / 60)*this.tenth_seconds%121;
+        let sec_angle = (Math.PI / 30)*this.get_seconds();
+        let min_angle = (Math.PI / 30)*this.get_mins();
+        this.analogue_display_hand(this.analogue_x, this.analogue_y, tenth_angle, this.radius, 1, '#985B00');
+        this.analogue_display_hand(this.analogue_x, this.analogue_y, sec_angle, this.radius, 2, '#DAB500');
+        this.analogue_display_hand(this.analogue_x, this.analogue_y, min_angle, this.radius*0.9, 3, '#AC6B00'); 
+        this.context.arc(this.analogue_x, this.analogue_y, 10, this.start_angle, this.end_angle);
+        this.context.fillStyle = '#B87900';
+        this.context.fill();
+    }
+    
+    digital_display() {
+        let watch_val = this.display_time();
+        this.context.font = '80px Arial';
+        this.context.strokeStyle = '#800E0E';
+        this.context.strokeText(watch_val, this.digital_x, this.digital_y, 1000, 1000);
+    }
 
-export {clock_face};
+    display_splits() {
+        console.log(this.splits);
+        if(this.splits.length > 0) {
+            for(let i=0; i<this.splits.length; i++) {
+                this.context.font = '80px Arial';
+                this.context.strokeStyle = '#800E0E';
+                this.context.strokeText(i+1, this.splits_x, this.splits_y+(i*100), 1000, 1000);
+                this.context.strokeText(this.splits[i], this.splits_x+100, this.splits_y+(i*100), 1000, 1000);
+            }
+        }
+    }
+
+    add_split() {
+        if(this.running) {
+            if(this.splits.length < 5) {
+                this.splits.push(this.display_time());
+            }
+            else {
+                this.splits = this.splits.splice(1, 5);
+                this.splits.push(this.display_time());
+            }
+        }
+    }
+    
+    draw_frame() {
+        this.context.clearRect(0, 0, this.canvas_width, this.canvas_height);
+        this.analogue_display();
+        this.digital_display();
+        this.display_splits();
+    }
+
+    animate_watch(){ if(this.running){ this.tick(); this.draw_frame();} }
+    start_watch(){ this.running = true; }
+    stop_watch(){ this.running = false; }
+    reset_watch(){ this.tenth_seconds = 0; this.splits=[]; this.draw_frame(); }
+
+}//end class
