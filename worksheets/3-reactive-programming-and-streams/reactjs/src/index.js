b8ce33c59e8bcaffc6de14b41f7daa19bc057dc9
Ronan <connollyro@gmail.com>
2018-11-13 20:11:48 +0000

    Basic webpack working with react

---
+++ worksheets/3-reactive-programming-and-streams/reactjs/src/index.js
@@ -0,0 +1,11 @@
+console.log('My Minimal React Webpack Babel Setup');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
